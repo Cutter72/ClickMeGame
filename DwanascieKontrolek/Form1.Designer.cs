4d769c4323d8bfe2f93f662e876b9c831ff53695
Cutter72 <drelich_pawel@o2.pl>
2020-02-01 13:12:03 +0100

    Initial commit

---
+++ DwanascieKontrolek/Form1.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace DwanascieKontrolek
+{
+    partial class ClickMeGame
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ClickMeGame));
+            this.clickMe = new System.Windows.Forms.Button();
+            this.radioBtnClickMeColorYellow = new System.Windows.Forms.RadioButton();
+            this.inputPlayerName = new System.Windows.Forms.TextBox();
+            this.highScoresTable = new System.Windows.Forms.RichTextBox();
+            this.progressBarTimer = new System.Windows.Forms.ProgressBar();
+            this.trackBarClickMeSizeChange = new System.Windows.Forms.TrackBar();
+            this.gameArena = new System.Windows.Forms.GroupBox();
+            this.settingsGroup = new System.Windows.Forms.GroupBox();
+            this.btnAcceptPlayerName = new System.Windows.Forms.Button();
+            this.labelTypeYourName = new System.Windows.Forms.Label();
+            this.labelClickMeColorChange = new System.Windows.Forms.Label();
+            this.labelClickMeSizeChange = new System.Windows.Forms.Label();
+            this.radioBtnClickMeColorNormal = new System.Windows.Forms.RadioButton();
+            this.highScoresGroup = new System.Windows.Forms.GroupBox();
+            this.howToPlayGroup = new System.Windows.Forms.GroupBox();
+            this.howToPlayDescription = new System.Windows.Forms.Label();
+            this.labelTimer = new System.Windows.Forms.Label();
+            this.scoreValue = new System.Windows.Forms.Label();
+            this.labelScore = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarClickMeSizeChange)).BeginInit();
+            this.gameArena.SuspendLayout();
+            this.settingsGroup.SuspendLayout();
+            this.highScoresGroup.SuspendLayout();
+            this.howToPlayGroup.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // clickMe
+            // 
+            this.clickMe.BackColor = System.Drawing.Color.Yellow;
+            this.clickMe.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.clickMe.Location = new System.Drawing.Point(225, 225);
+            this.clickMe.Name = "clickMe";
+            this.clickMe.Size = new System.Drawing.Size(40, 40);
+            this.clickMe.TabIndex = 0;
+            this.clickMe.Text = "Start";
+            this.clickMe.UseVisualStyleBackColor = false;
+            this.clickMe.MouseClick += new System.Windows.Forms.MouseEventHandler(this.clickMe_MouseClick);
+            // 
+            // radioBtnClickMeColorYellow
+            // 
+            this.radioBtnClickMeColorYellow.AutoSize = true;
+            this.radioBtnClickMeColorYellow.Checked = true;
+            this.radioBtnClickMeColorYellow.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.radioBtnClickMeColorYellow.Location = new System.Drawing.Point(9, 71);
+            this.radioBtnClickMeColorYellow.Name = "radioBtnClickMeColorYellow";
+            this.radioBtnClickMeColorYellow.Size = new System.Drawing.Size(83, 17);
+            this.radioBtnClickMeColorYellow.TabIndex = 3;
+            this.radioBtnClickMeColorYellow.TabStop = true;
+            this.radioBtnClickMeColorYellow.Text = "Yellow (+2p)";
+            this.radioBtnClickMeColorYellow.UseVisualStyleBackColor = true;
+            this.radioBtnClickMeColorYellow.CheckedChanged += new System.EventHandler(this.radioBtnClickMeColorYellow_CheckedChanged);
+            // 
+            // inputPlayerName
+            // 
+            this.inputPlayerName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.inputPlayerName.Location = new System.Drawing.Point(9, 32);
+            this.inputPlayerName.MaxLength = 16;
+            this.inputPlayerName.Name = "inputPlayerName";
+            this.inputPlayerName.Size = new System.Drawing.Size(90, 20);
+            this.inputPlayerName.TabIndex = 4;
+            this.inputPlayerName.KeyDown += new System.Windows.Forms.KeyEventHandler(this.inputPlayerName_KeyDown);
+            // 
+            // highScoresTable
+            // 
+            this.highScoresTable.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.highScoresTable.Location = new System.Drawing.Point(6, 19);
+            this.highScoresTable.Name = "highScoresTable";
+            this.highScoresTable.ReadOnly = true;
+            this.highScoresTable.Size = new System.Drawing.Size(189, 199);
+            this.highScoresTable.TabIndex = 5;
+            this.highScoresTable.Text = "";
+            // 
+            // progressBarTimer
+            // 
+            this.progressBarTimer.Location = new System.Drawing.Point(10, 518);
+            this.progressBarTimer.Maximum = 1000;
+            this.progressBarTimer.Name = "progressBarTimer";
+            this.progressBarTimer.Size = new System.Drawing.Size(421, 20);
+            this.progressBarTimer.Step = 1;
+            this.progressBarTimer.TabIndex = 6;
+            this.progressBarTimer.Value = 1000;
+            // 
+            // trackBarClickMeSizeChange
+            // 
+            this.trackBarClickMeSizeChange.Location = new System.Drawing.Point(6, 112);
+            this.trackBarClickMeSizeChange.Maximum = 3;
+            this.trackBarClickMeSizeChange.Name = "trackBarClickMeSizeChange";
+            this.trackBarClickMeSizeChange.Size = new System.Drawing.Size(185, 45);
+            this.trackBarClickMeSizeChange.TabIndex = 10;
+            this.trackBarClickMeSizeChange.Value = 2;
+            this.trackBarClickMeSizeChange.Scroll += new System.EventHandler(this.trackBarClickMeSizeChange_Scroll);
+            // 
+            // gameArena
+            // 
+            this.gameArena.Controls.Add(this.clickMe);
+            this.gameArena.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.gameArena.Location = new System.Drawing.Point(10, 10);
+            this.gameArena.Name = "gameArena";
+            this.gameArena.Size = new System.Drawing.Size(500, 500);
+            this.gameArena.TabIndex = 11;
+            this.gameArena.TabStop = false;
+            this.gameArena.Text = "Game Arena";
+            // 
+            // settingsGroup
+            // 
+            this.settingsGroup.Controls.Add(this.inputPlayerName);
+            this.settingsGroup.Controls.Add(this.btnAcceptPlayerName);
+            this.settingsGroup.Controls.Add(this.labelTypeYourName);
+            this.settingsGroup.Controls.Add(this.labelClickMeColorChange);
+            this.settingsGroup.Controls.Add(this.labelClickMeSizeChange);
+            this.settingsGroup.Controls.Add(this.trackBarClickMeSizeChange);
+            this.settingsGroup.Controls.Add(this.radioBtnClickMeColorNormal);
+            this.settingsGroup.Controls.Add(this.radioBtnClickMeColorYellow);
+            this.settingsGroup.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.settingsGroup.Location = new System.Drawing.Point(516, 118);
+            this.settingsGroup.Name = "settingsGroup";
+            this.settingsGroup.Size = new System.Drawing.Size(200, 162);
+            this.settingsGroup.TabIndex = 12;
+            this.settingsGroup.TabStop = false;
+            this.settingsGroup.Text = "Settings";
+            // 
+            // btnAcceptPlayerName
+            // 
+            this.btnAcceptPlayerName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnAcceptPlayerName.Location = new System.Drawing.Point(101, 32);
+            this.btnAcceptPlayerName.Name = "btnAcceptPlayerName";
+            this.btnAcceptPlayerName.Size = new System.Drawing.Size(90, 20);
+            this.btnAcceptPlayerName.TabIndex = 0;
+            this.btnAcceptPlayerName.Text = "Accept";
+            this.btnAcceptPlayerName.UseVisualStyleBackColor = true;
+            this.btnAcceptPlayerName.Click += new System.EventHandler(this.btnAcceptPlayerName_Click);
+            // 
+            // labelTypeYourName
+            // 
+            this.labelTypeYourName.AutoSize = true;
+            this.labelTypeYourName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelTypeYourName.Location = new System.Drawing.Point(3, 16);
+            this.labelTypeYourName.Name = "labelTypeYourName";
+            this.labelTypeYourName.Size = new System.Drawing.Size(61, 13);
+            this.labelTypeYourName.TabIndex = 2;
+            this.labelTypeYourName.Text = "Your name:";
+            // 
+            // labelClickMeColorChange
+            // 
+            this.labelClickMeColorChange.AutoSize = true;
+            this.labelClickMeColorChange.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelClickMeColorChange.Location = new System.Drawing.Point(3, 55);
+            this.labelClickMeColorChange.Name = "labelClickMeColorChange";
+            this.labelClickMeColorChange.Size = new System.Drawing.Size(116, 13);
+            this.labelClickMeColorChange.TabIndex = 2;
+            this.labelClickMeColorChange.Text = "\'Click me!\' button color:";
+            // 
+            // labelClickMeSizeChange
+            // 
+            this.labelClickMeSizeChange.AutoSize = true;
+            this.labelClickMeSizeChange.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelClickMeSizeChange.Location = new System.Drawing.Point(3, 92);
+            this.labelClickMeSizeChange.Name = "labelClickMeSizeChange";
+            this.labelClickMeSizeChange.Size = new System.Drawing.Size(138, 13);
+            this.labelClickMeSizeChange.TabIndex = 2;
+            this.labelClickMeSizeChange.Text = "\'Click me!\' button size (+3p):";
+            // 
+            // radioBtnClickMeColorNormal
+            // 
+            this.radioBtnClickMeColorNormal.AutoSize = true;
+            this.radioBtnClickMeColorNormal.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.radioBtnClickMeColorNormal.Location = new System.Drawing.Point(101, 71);
+            this.radioBtnClickMeColorNormal.Name = "radioBtnClickMeColorNormal";
+            this.radioBtnClickMeColorNormal.Size = new System.Drawing.Size(85, 17);
+            this.radioBtnClickMeColorNormal.TabIndex = 3;
+            this.radioBtnClickMeColorNormal.Text = "Normal (+3p)";
+            this.radioBtnClickMeColorNormal.UseVisualStyleBackColor = true;
+            this.radioBtnClickMeColorNormal.CheckedChanged += new System.EventHandler(this.radioBtnClickMeColorNormal_CheckedChanged);
+            // 
+            // highScoresGroup
+            // 
+            this.highScoresGroup.Controls.Add(this.highScoresTable);
+            this.highScoresGroup.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.highScoresGroup.Location = new System.Drawing.Point(516, 286);
+            this.highScoresGroup.Name = "highScoresGroup";
+            this.highScoresGroup.Size = new System.Drawing.Size(200, 224);
+            this.highScoresGroup.TabIndex = 12;
+            this.highScoresGroup.TabStop = false;
+            this.highScoresGroup.Text = "High Scores";
+            // 
+            // howToPlayGroup
+            // 
+            this.howToPlayGroup.Controls.Add(this.howToPlayDescription);
+            this.howToPlayGroup.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.howToPlayGroup.Location = new System.Drawing.Point(516, 10);
+            this.howToPlayGroup.Name = "howToPlayGroup";
+            this.howToPlayGroup.Size = new System.Drawing.Size(200, 102);
+            this.howToPlayGroup.TabIndex = 13;
+            this.howToPlayGroup.TabStop = false;
+            this.howToPlayGroup.Text = "How to play?";
+            // 
+            // howToPlayDescription
+            // 
+            this.howToPlayDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.howToPlayDescription.Location = new System.Drawing.Point(9, 16);
+            this.howToPlayDescription.Name = "howToPlayDescription";
+            this.howToPlayDescription.Size = new System.Drawing.Size(182, 84);
+            this.howToPlayDescription.TabIndex = 2;
+            this.howToPlayDescription.Text = resources.GetString("howToPlayDescription.Text");
+            // 
+            // labelTimer
+            // 
+            this.labelTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelTimer.Location = new System.Drawing.Point(437, 518);
+            this.labelTimer.Name = "labelTimer";
+            this.labelTimer.Size = new System.Drawing.Size(73, 20);
+            this.labelTimer.TabIndex = 2;
+            this.labelTimer.Text = "10.00 s";
+            this.labelTimer.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // scoreValue
+            // 
+            this.scoreValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.scoreValue.Location = new System.Drawing.Point(617, 519);
+            this.scoreValue.Name = "scoreValue";
+            this.scoreValue.Size = new System.Drawing.Size(99, 20);
+            this.scoreValue.TabIndex = 2;
+            this.scoreValue.Text = "0 p";
+            this.scoreValue.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelScore
+            // 
+            this.labelScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelScore.Location = new System.Drawing.Point(521, 518);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(94, 21);
+            this.labelScore.TabIndex = 2;
+            this.labelScore.Text = "Score:";
+            this.labelScore.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // ClickMeGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(726, 548);
+            this.Controls.Add(this.howToPlayGroup);
+            this.Controls.Add(this.scoreValue);
+            this.Controls.Add(this.highScoresGroup);
+            this.Controls.Add(this.labelScore);
+            this.Controls.Add(this.labelTimer);
+            this.Controls.Add(this.settingsGroup);
+            this.Controls.Add(this.progressBarTimer);
+            this.Controls.Add(this.gameArena);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "ClickMeGame";
+            this.Text = "Click me! - Game";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarClickMeSizeChange)).EndInit();
+            this.gameArena.ResumeLayout(false);
+            this.settingsGroup.ResumeLayout(false);
+            this.settingsGroup.PerformLayout();
+            this.highScoresGroup.ResumeLayout(false);
+            this.howToPlayGroup.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button clickMe;
+        private System.Windows.Forms.RadioButton radioBtnClickMeColorYellow;
+        private System.Windows.Forms.TextBox inputPlayerName;
+        private System.Windows.Forms.RichTextBox highScoresTable;
+        private System.Windows.Forms.ProgressBar progressBarTimer;
+        private System.Windows.Forms.TrackBar trackBarClickMeSizeChange;
+        private System.Windows.Forms.GroupBox gameArena;
+        private System.Windows.Forms.GroupBox settingsGroup;
+        private System.Windows.Forms.GroupBox highScoresGroup;
+        private System.Windows.Forms.Button btnAcceptPlayerName;
+        private System.Windows.Forms.Label labelClickMeColorChange;
+        private System.Windows.Forms.Label labelClickMeSizeChange;
+        private System.Windows.Forms.RadioButton radioBtnClickMeColorNormal;
+        private System.Windows.Forms.Label labelTypeYourName;
+        private System.Windows.Forms.GroupBox howToPlayGroup;
+        private System.Windows.Forms.Label howToPlayDescription;
+        private System.Windows.Forms.Label labelTimer;
+        private System.Windows.Forms.Label scoreValue;
+        private System.Windows.Forms.Label labelScore;
+    }
+}
+
---
+++ DwanascieKontrolek/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace DwanascieKontrolek
+{
+    public partial class ClickMeGame : Form
+    {
+        //Pola
+        private const int minX = 0;
+        private const int minY = 12;
+        private int gameTimeMillis;
+        private int score;
+        private int clickCounter;
+        private int sizePoints;
+        private int colorPoints;
+        private string playerName;
+        private Point newLocation;
+        private Size newSize;
+        private Random random;
+        private System.Windows.Forms.Timer timer;
+
+        //Konstruktor w którym inicjalizujemy niezainicjalizowane zmienne
+        public ClickMeGame()
+        {
+            InitializeComponent();
+            this.MaximizeBox = false;
+            gameTimeMillis = 10000;
+            sizePoints = 3;
+            colorPoints = 2;
+            newSize = new Size(40, 40);
+            random = new Random();
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = 10;
+            timer.Tick += TimerTickAction;
+            resetClickMeButtonLocation();
+        }
+
+        //Wyśrodkowanie przycisku
+        private void resetClickMeButtonLocation()
+        {
+            newLocation.X = gameArena.Size.Width / 2 - clickMe.Size.Width / 2;
+            newLocation.Y = gameArena.Size.Height / 2 - clickMe.Size.Height / 2;
+            clickMe.Location = newLocation;
+        }
+
+        /* Zmiana położenia przycisku w zależności od rozmiaru tak aby
+           nie wyszedł poza obszar Game Arena */
+        private void changeClickMeLocation()
+        {
+            setClickMeButtonRandomLocation(gameArena.Size.Width - clickMe.Size.Width);
+        }
+
+        //Losowanie współrzędnych przycisku i ustawienie ich
+        private void setClickMeButtonRandomLocation(int maxValue)
+        {
+            newLocation.X = random.Next(minX, maxValue);
+            newLocation.Y = random.Next(minY, maxValue);
+            clickMe.Location = newLocation;
+        }
+
+        //Metoda wywoływana przez Timer co ustawiony interwał czasowy
+        private void TimerTickAction(object sender, EventArgs e)
+        {
+            if (gameTimeMillis > 0)
+            {
+                gameTimeMillis -= 10;
+                progressBarTimer.Value -= 1;
+                labelTimer.Text = $"{gameTimeMillis / 1000d} s";
+            }
+            else
+            {
+                timer.Stop();
+                clickMe.Text = "Reset";
+                highScoresTable.AppendText($"{score} points, " +
+                    $"{clickCounter} clicks - {playerName}\n");
+                MessageBox.Show($"Well done {playerName}!\nYour score is {score}" +
+                    $" points with {clickCounter} clicks!", "Time has run out!");
+            }
+        }
+
+        //Zmiana koloru na żółty (domyślny)
+        private void radioBtnClickMeColorYellow_CheckedChanged(object sender, EventArgs e)
+        {
+            colorPoints = 2;
+            clickMe.BackColor = Color.Yellow;
+        }
+        //Zmiana koloru na normalny (szary)
+        private void radioBtnClickMeColorNormal_CheckedChanged(object sender, EventArgs e)
+        {
+            colorPoints = 3;
+            clickMe.BackColor = Color.FromArgb(240, 240, 240);
+        }
+
+        //Obsługa kliknięcia przycisku Accept/Change
+        private void btnAcceptPlayerName_Click(object sender, EventArgs e)
+        {
+            if (inputPlayerName.Enabled)
+            {
+                playerName = inputPlayerName.Text;
+                inputPlayerName.Enabled = false;
+                btnAcceptPlayerName.Text = "Change";
+            }
+            else
+            {
+                inputPlayerName.Enabled = true;
+                btnAcceptPlayerName.Text = "Accept";
+            }
+        }
+        //Obsługa naciśnięcia klawisza Enter w polu textowym
+        private void inputPlayerName_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnAcceptPlayerName_Click(sender, e);
+            }
+        }
+
+        //Obsługa zmiany rozmiaru
+        private void trackBarClickMeSizeChange_Scroll(object sender, EventArgs e)
+        {
+            switch (trackBarClickMeSizeChange.Value)
+            {
+                case 0:
+                    {
+                        setNewClickMeButtonSize(80, 1);
+                        break;
+                    }
+                case 1:
+                    {
+                        setNewClickMeButtonSize(60, 2);
+                        break;
+                    }
+                case 2:
+                    {
+                        setNewClickMeButtonSize(40, 3);
+                        break;
+                    }
+                case 3:
+                    {
+                        setNewClickMeButtonSize(20, 4);
+                        break;
+                    }
+            }
+        }
+        //Metoda zmiany rozmiaru
+        private void setNewClickMeButtonSize(int size, int pointsForSize)
+        {
+            newSize.Width = size;
+            newSize.Height = size;
+            clickMe.Size = newSize;
+            sizePoints = pointsForSize;
+            labelClickMeSizeChange.Text = $"'Click me!' button size (+{sizePoints}p):";
+            resetClickMeButtonLocation();
+        }
+
+        //Obsługa przycisku Click me!
+        private void clickMe_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (clickMe.Text.Equals("Start"))
+            {
+                if (playerName == null)
+                {
+                    MessageBox.Show("Please enter your name in Settings " +
+                        "and click Accept or hit Enter!", "No name entered!");
+                }
+                else if (playerName.Equals(""))
+                {
+                    MessageBox.Show("Please enter your name in Settings " +
+                        "and click Accept or hit Enter!", "No name entered!");
+                }
+                else
+                {
+                    //Akcja startująca grę gdy imie gracza zostało wprowadzone
+                    startGame();
+                }
+            }
+            else if (clickMe.Text.Equals("Click me!"))
+            {
+                //Akcja podczas trwania gry
+                updateScoreAndMoveToRandomLocation();
+            }
+            else
+            {
+                //Akcja po wyczerpaniu limitu czasowego
+                resetGame();
+            }
+        }
+
+        //Akcja przycisku podczas trwania gry
+        private void updateScoreAndMoveToRandomLocation()
+        {
+            clickCounter++;
+            score = clickCounter * (colorPoints + sizePoints);
+            scoreValue.Text = $"{score} p";
+            changeClickMeLocation();
+        }
+
+        //Akcja przycisku startująca grę gdy imie gracza zostało wprowadzone
+        private void startGame()
+        {
+            timer.Start();
+            clickMe.Text = "Click me!";
+            changeClickMeLocation();
+        }
+
+        //Zresetowanie parametrów gry do stanu początkowego
+        private void resetGame()
+        {
+            clickMe.Text = "Start";
+            score = 0;
+            clickCounter = 0;
+            gameTimeMillis = 10000;
+            progressBarTimer.Value = 1000;
+            labelTimer.Text = $"{gameTimeMillis / 1000d} s";
+            scoreValue.Text = $"{score} p";
+            resetClickMeButtonLocation();
+        }
+    }
+}
